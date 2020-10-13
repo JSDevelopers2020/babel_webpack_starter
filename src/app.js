b034db8d847f1adea718857fe9bf0796efbf0554
Raniel Garcia <raniel12345@users.noreply.github.com>
2020-10-13 09:44:22 +0700

    Add async-await getPosts Method
    Add async-await getPosts Method

--- src/app.js
+++ src/app.js
@@ -13,3 +13,13 @@ const getData = async (url) => {
 };
 
 getData('https://jsonplaceholder.typicode.com/posts');
+
+// ---------------------------
+
+async function getPosts2 (){
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+getPosts2().then(posts => console.log(posts));
+// this will require babel-polyfill babel-preset-stage-0
